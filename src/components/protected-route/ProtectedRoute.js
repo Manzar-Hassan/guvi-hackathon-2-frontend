d4521fbe52b6847f6cbe931186deed58d6a8dd5c
Manzar Hassan <manzarhassan05@gmail.com>
2022-08-06 20:27:36 +0530

    console.log removed

--- src/components/protected-route/ProtectedRoute.js
+++ src/components/protected-route/ProtectedRoute.js
@@ -3,11 +3,10 @@ import { Navigate } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 
 function ProtectedRoute({ children }) {
-  const {isLoggedIn} = useContext(UserContext)
+  const { isLoggedIn } = useContext(UserContext);
 
-  console.log(isLoggedIn)
   if (!isLoggedIn) {
-    return <Navigate to={"/"} replace />
+    return <Navigate to={"/"} replace />;
   }
 
   return children;
